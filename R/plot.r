bdc2c79421ba49de20d32144cddeb61682cf8108
lionup <lionup@gmail.com>
2014-03-27 17:43:03 +0000

    hello

--- R/plot.r
+++ R/plot.r
@@ -1,5 +1,34 @@
 setwd('figure')
 require(ggplot2)   
+age = seq(30, (30+(nage-1)*2), 2)
+
+plot_median <- data.frame( cbind(age=age, income=xeta[,(nbin+1)/2]) )
+
+p_median  <- ggplot(plot_median, aes(x=age,y=income)) + 
+  geom_line(aes(color='median')) +
+  #geom_line(aes(y=consumption, color='consumption')) +
+  xlab('age') +
+  ylab('persistent income')+
+  labs(colour = NULL) +
+  #ylab('consumption') +
+ # ggtitle('life cycle profile of permanent income with constrained grid upper bound') +
+  theme_bw()
+
+print(p_median)
+
+ggsave('perm_inc_bigData.png',width=10.6, height=5.93)  
+setwd('~/git/abbg/R')
+
+
+
+
+
+
+
+
+######################################################################
+setwd('figure')
+require(ggplot2)   
 age = 30:(30+p$nt-1)
 plot_mean <- data.frame( cbind(age=age, income=etalong/p$nsim) )
 
@@ -19,7 +48,7 @@ ggsave('perm_inc.png',width=10.6, height=5.93)
 setwd('~/git/abbg/R')
 
 
-
+####################################################
 
 setwd('figure')
 require(ggplot2)   
