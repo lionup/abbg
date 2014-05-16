7ab86c4e4c837fbcd312a1fed0e037ee621f80de
lionup <lionup@gmail.com>
2014-05-16 20:06:16 +0100

    rq

--- R/temp.r
+++ R/temp.r
@@ -1,45 +1,45 @@
 sim.qr <- function(p, sim){
-  Y=sim$eta+sim$eps;
 
-Vect=Y(:,1:T-1);
-Vect_dep=Y(:,2:T);
+sim$t <-1:6 
+sim_y <- sim[,c("id","t","Y"),with=F]
+wide_y <- reshape(sim_y, idvar='id', timevar='t', direction='wide') #each person has all age in a row
 
-Mat1=[];
-for kk1=0:K1
+Vect=Y[,1:T-1]
+Vect_dep=Y[,2:T]
+
+Mat1<- matrix( 0, nrow=length(Vect),ncol=(K1+1) )
+for (kk1 in 0:K1){
+  Mat1[,kk1+1]=hermite( (c(Vect)-mean(Y))/sd(Y),kk1 )
+}
+
+ResP_data=array( 0,dim=c((K1+1),Ntau) )
+
+require(quantreg)
+for (jtau in 1:Ntau){
     
-        Mat1=[Mat1 hermite(kk1,(Vect(:)-meanY)/stdY)];
+    tau=Vectau[jtau]
     
-end
-
-ResP_data=zeros((K1+1),Ntau);
-
-for jtau=1:Ntau
+    ResP_data[,jtau]=rq(c(Vect_dep)~Mat1-1,tau)$coefficients
     
-    tau=Vectau(jtau);
-    
-    ResP_data(:,jtau)=rq(Mat1,Vect_dep(:),tau);
-    
-end
+}
 
-% Covariates (to compute the derivative of the quantile function with respect to y_{t-1})
+# Covariates (to compute the derivative of the quantile function with respect to y_{t-1})
 
-Vect=quantile(Vect(:),Vectau);
+Vect=quantile(c(Vect),Vectau, names=F, na.rm = T )
 
-Mat3=zeros(Ntau,1);
-for kk1=1:K1
-    
-        Mat3=[Mat3 kk1*hermite(kk1-1,(Vect(:)-meanY)/stdY)./stdY];
-   
-end
+Mat3=matrix( 1, nrow=Ntau,ncol=K1+1 )
+for (kk1 in 1:K1){
+  Mat3[,kk1+1] = kk1 * hermite( (c(Vect)-mean(Y))/sd(Y),kk1-1 ) / sd(Y)
+}
 
-% Matrix of persistence
+# Matrix of persistence
 
-Mat3*ResP_data
+persis <- Mat3 %*% ResP_data
 
-% Drawing the graph 
+# Drawing the graph 
 
-figure
-surf(Vectau,Vectau,Mat3*ResP_data)
+require(plot3D)
+persp3D(Vectau,Vectau,persis)
 axis([0 1 0 1 0 1.2])
 xlabel('percentile \tau_{shock}')
 ylabel('percentile \tau_{init}')
