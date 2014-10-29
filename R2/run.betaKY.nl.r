201a764a6c3506a137c95c597a4023bb327b2ce0
lionup <lionup@gmail.com>
2014-10-29 14:08:05 +0000

    d

--- R2/run.betaKY.nl.r
+++ R2/run.betaKY.nl.r
@@ -51,7 +51,7 @@ p$gam =   2
 p$bet =   1/p$R
 
 #BORROWING LIMIT: SET TO VERY LARGE NEGATIVE NO. FOR NBL
-p$borrowlim = 0 #0.0 #-100000000.0
+p$borrowlim = -100000000.0 #0.0 #-100000000.0
 
 #GOVERNMENT PARAMETERS
 #gouveia strauss 
@@ -68,7 +68,7 @@ p$pencapfrac = 2.2 #cap on (pre-tax) earnings that contribute to pension index,
 p$Rnet = 1.0 + (1.0-p$rtax)*(p$R-1)      #annual after tax interest rate
 
 #OPTIONS
-p$Display  = 1
+p$Display  = 0 #1
 p$mode <- 'multicore' #'serial' #'multicore' #'mpi' 
 
 #find beta by matching KY ratio
@@ -80,7 +80,7 @@ cat(' Beta after: ',p$bet, '\n')
 
 #use new bet to compute moments
 moments  <- comp.solveModel(p)
-save(p, moments, file='nl_zbl.dat') 
+save(p, moments, file='nl_nbl.dat') 
 
 cat(paste('\ntotal seconds to solve the program: ' , proc.time()[3] -  start_time ))
 
